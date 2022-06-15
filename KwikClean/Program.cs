695200981065f6e5de64b7b441ba5d36b6636e22
Dan Banfield <dandbanfield@gmail.com>
2022-06-15 17:58:09 +0100

    Added code to handle user input.

--- KwikClean/Program.cs
+++ KwikClean/Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KwikClean
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            SetupConsole();
+            ShowMenu();
+        }
 
+        private static void SetupConsole()
+        {
+            Console.Title = "KwikClean";
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void ShowMenu()
+        {
+            Console.Write("Would you like to remove all unwanted files on your PC to free up disk space? (This may take a while.) Y/N: ");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "y") BeginCleanupProcess();
+            else Environment.Exit(0);
+        }
+
+        private static void BeginCleanupProcess()
+        {
+            // TODO: Add code to remove unwanted files.
         }
     }
 }
